a9241299519aaf0e541040d066eeaef6d2847f15
WAYINDIA <developers@wayindia.com>
2023-09-17 09:26:53 +0530

    Course List section updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,10 +30,17 @@ class HomePage extends StatelessWidget {
   ];
 
   List imgList = [
+    'c_sharp',
+    'react_native',
+    'Python',
     'Flutter',
+  ];
+
+  List courseList = [
+    'C#',
     'React Native',
     'Python',
-     'C#',
+    'Flutter',
   ];
 
   @override
@@ -184,6 +191,7 @@ class HomePage extends StatelessWidget {
                 ),
                 SizedBox(height: 10),
                 GridView.builder(
+                  itemCount: imgList.length,
                   shrinkWrap: true,
                   physics: NeverScrollableScrollPhysics(),
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -194,6 +202,49 @@ class HomePage extends StatelessWidget {
                     mainAxisSpacing: 10,
                     crossAxisSpacing: 10,
                   ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xFFF5F3FF),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                "images/${imgList[index]}.png",
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              courseList[index],
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black.withOpacity(0.6),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              '55 Videos',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
