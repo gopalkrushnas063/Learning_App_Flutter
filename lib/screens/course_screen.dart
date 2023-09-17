e8220f5512dab09fe77b5758cc2d15a801800069
WAYINDIA <developers@wayindia.com>
2023-09-17 10:39:08 +0530

    in course section added two button video and description

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CourseScreen extends StatefulWidget {
-  
   String img;
   CourseScreen(this.img);
 
@@ -10,8 +9,160 @@ class CourseScreen extends StatefulWidget {
 }
 
 class _CourseScreenState extends State<CourseScreen> {
+  bool isVideoSection = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          widget.img,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.notifications,
+              size: 28,
+              color: Color(0xFF674AEF),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFFF5F3FF),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/${widget.img}.png')),
+              ),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.play_arrow_rounded,
+                    color: Color(0xFF674AEF),
+                    size: 45,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              '${widget.img} Complete Course',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Created by Gopal Krishna',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.black.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              '55 Videos',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F3FF),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Material(
+                    // if isVideoSection condition true then that color will be on button and if condition is false then that color wil be shown on button but with some opacity.
+                    color: isVideoSection
+                        ? Color(0xFF674AEF)
+                        : Color(0xFF674AEF).withOpacity(0.6),
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        //change value of isVideoSection
+                        setState(() {
+                          isVideoSection = true;
+                        });
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                        child: Text(
+                          'Videos',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    color: isVideoSection
+                        ? Color(0xFF674AEF).withOpacity(0.5)
+                        : Color(0xFF674AEF),
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        //change value of isVideoSection
+                        setState(() {
+                          isVideoSection = false;
+                        });
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                        child: Text(
+                          'Description',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Create for both description and video section
+            SizedBox(height: 10),
+            isVideoSection ? VideoSection() : DescriptionSection(),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
