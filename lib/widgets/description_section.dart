71d9246c9d042374b46caeb4440e6326f5319584
WAYINDIA <developers@wayindia.com>
2023-09-18 06:57:01 +0530

    Description section updated

--- lib/widgets/description_section.dart
+++ lib/widgets/description_section.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
 
-
 class DescriptionSection extends StatelessWidget {
   const DescriptionSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text(
+            "E-learning is a digital educational approach that utilizes internet-based technologies to deliver educational content and facilitate learning. It offers flexibility, accessibility, and a wide range of multimedia resources for learners, making it a popular choice for remote and self-paced education.",
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black.withOpacity(0.7),
+            ),
+            textAlign: TextAlign.justify,
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text(
+                "Course Length: ",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 5),
+              Icon(
+                Icons.timer,
+                color: Color(0xFF674AEF),
+              ),
+              SizedBox(width: 5),
+              Text(
+                "26 Hours",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "4.5",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
