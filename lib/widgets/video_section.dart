dbfd3ed357124f6a4b578a1b5090bd39e054a2d1
WAYINDIA <developers@wayindia.com>
2023-09-18 06:40:00 +0530

    title and subtitle added in video list item

--- lib/widgets/video_section.dart
+++ lib/widgets/video_section.dart
@@ -30,6 +30,10 @@ class VideoSection extends StatelessWidget {
               size: 30,
             ),
           ),
+          title: Text(
+            videoList[index],
+          ),
+          subtitle: Text("20 min 50 sec"),
         );
       },
     );
